b1594623ee8e4aeed945dad7a6b6966299de347a
matveykylak <matveykylak@gmail.com>
2024-06-19 19:11:59 +0300

    Add project files.

---
+++ PageObjectLib/Elements/WebElement.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using PageObjectLib.Factories;
+
+namespace PageObjectLib.Elements
+{
+    public class WebElement
+    {
+        private static readonly IWebDriver _driver = Driver.GetDriver();
+        private static readonly WebDriverWait _wait = Driver.GetWait();
+        private readonly By? _locator;
+        private static Actions _actions = new Actions(_driver);
+
+        public WebElement(By locator) => _locator = locator;
+
+        public IWebElement Element
+        {
+            get
+            {
+                WaitWebElementPresent();
+                return _driver.FindElement(_locator);
+            }
+        }
+
+        public void WaitWebElementPresent() => _wait.Until(drv => drv.FindElements(_locator).Count() > 0);
+
+        public void SendValue(string value)
+        {
+            ScrollToElementByJS();
+            Element.SendKeys(value);
+        }
+
+        public void Click()
+        {
+            ScrollToElementByJS();
+            Element.Click();
+        }
+
+        public void ScrollToElement() => _actions.MoveToElement(Element);
+
+        public void ScrollToElementByJS() => ((IJavaScriptExecutor)_driver).ExecuteScript("argument[0].scrollIntoView(true)", Element);
+
+        public string GetAttribute(string atr) => Element.GetAttribute(atr);
+
+        public string GetText() => Element.Text;
+
+        public static void AcceptAlert()
+        {
+            var alert = _driver.SwitchTo().Alert();
+            alert.Accept();
+        }
+
+        public static bool IsalertShown()
+        {
+            try
+            {
+                _driver.SwitchTo().Alert();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void SwitchToFrame()
+        {
+            _driver.SwitchTo().Frame(Element);
+        }
+
+        public void FrameExit() => _driver.SwitchTo().DefaultContent();
+    }
+}
---
+++ PageObjectLib/Factories/Driver.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObjectLib.Factories
+{
+    public static class Driver
+    {
+        private static IWebDriver? _driver;
+        private static WebDriverWait? _wait;
+        private static ChromeOptions? _chromeOptions;
+        private static Actions? _actions = new Actions(_driver);
+
+        public static WebDriverWait GetWait() => _wait ??= new(_driver, TimeSpan.FromSeconds(60));
+        public static IWebDriver GetDriver() => _driver ??= new ChromeDriver(GetOptions());
+        public static ChromeOptions GetOptions()
+        {
+            if (_chromeOptions == null)
+            {
+                _chromeOptions = new ChromeOptions();
+                _chromeOptions.AddArgument("start-maximized");
+            }
+            return _chromeOptions;
+        }
+        public static void QuitDriver()
+        {
+            _driver?.Quit();
+            _driver = null;
+            _wait = null;
+            _actions = null;
+        }
+        public static void DisposeDriver() => _driver?.Dispose();
+    }
+}
